34c1cad0b03737f2ea466e5058f8ff872db5adc0
PrinzJuliano <PrinzJuliano@users.noreply.github.com>
2016-07-03 00:11:53 +0200

    Updated the Java Doc for having two new Views. JavaDoc will only be updated once in a while.

---
+++ Java Sources/src/main/java/de/pjog/prinzJuliano/TerminalRPG/views/SettingsView.java
@@ -0,0 +1,20 @@
+package de.pjog.prinzJuliano.TerminalRPG.views;
+
+import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
+
+import de.pjog.prinzJuliano.TerminalRPG.Storyboard;
+
+/**
+ * A view containing all the available settings.
+ * @author PrinzJuliano
+ *
+ */
+public class SettingsView extends AbstractView {
+
+	@Override
+	public void init(Storyboard story, WindowBasedTextGUI textGUI) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
