228b8ef25f4988ba1b3746a7b10bb8ee817c20db
PrinzJuliano <PrinzJuliano@users.noreply.github.com>
2016-08-21 15:54:49 +0200

    Added proper preview pictures and switching from one to another.

--- Java Sources/src/main/java/de/pjog/prinzJuliano/TerminalRPG/views/LoadingDialog.java
+++ Java Sources/src/main/java/de/pjog/prinzJuliano/TerminalRPG/views/LoadingDialog.java
@@ -1,61 +1,44 @@
 package de.pjog.prinzJuliano.TerminalRPG.views;
 
-import com.googlecode.lanterna.gui2.Button;
 import com.googlecode.lanterna.gui2.Direction;
 import com.googlecode.lanterna.gui2.Label;
 import com.googlecode.lanterna.gui2.LinearLayout;
 import com.googlecode.lanterna.gui2.Panel;
-import com.googlecode.lanterna.gui2.Separator;
 import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
 
 import de.pjog.prinzJuliano.TerminalRPG.Storyboard;
 
-/**
- * A basic Dialog.
- * @author PrinzJuliano
- *
- */
-public class Dialog extends AbstractView {
-
+public class LoadingDialog extends AbstractView{
 	private int nextView;
-	private String message;
 	private String communication;
-	private String caption;
-	
-	/**
-	 * Convenience Constructor.
-	 */
-	public Dialog(){
-		super();
-	}
+
 	
 	@Override
 	public void init(final Storyboard story, WindowBasedTextGUI textGUI) {
 		textGUI.getBackgroundPane().setComponent(new Background());
-		rootWindow.setTitle(caption);
+		rootWindow.setTitle("Loading . . .");
 		
 		Panel p = new Panel();
 		p.setLayoutManager(new LinearLayout(Direction.VERTICAL));
 		
-		Label error = new Label(message);
-		p.addComponent(error);
-		
-		Button close = new Button("Ok", new Runnable() {
-			
-			@Override
-			public void run() {
-				story.switchToView(nextView, communication);
-				
-			}
-		});
-		p.addComponent(new Separator(Direction.HORIZONTAL));
-		p.addComponent(close);
+		Label text = new Label("The next section is loading!");
+		p.addComponent(text);
 		
 		rootWindow.setComponent(p);
 		
 		textGUI.addWindow(rootWindow);
 		
 		onResize(textGUI.getScreen().getTerminalSize());
+		
+		new java.util.Timer().schedule( 
+		        new java.util.TimerTask() {
+		            @Override
+		            public void run() {
+		            	story.switchToView(nextView, communication);
+		            }
+		        }, 
+		        500 
+		);
 	}
 
 	@Override
@@ -73,21 +56,6 @@ public class Dialog extends AbstractView {
 		this.nextView = nextView;
 	}
 
-	/**
-	 * Set the message that should be displayed.
-	 * @param message the message.
-	 */
-	public void setMessage(String message) {
-		this.message = message;
-	}
 	
-	/**
-	 * Set the Caption of this Dialog.
-	 * @param caption the caption.
-	 */
-	public void setCaption(String caption) {
-		this.caption = caption;
-	}
-	
-	
+
 }
