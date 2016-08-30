9610cc207842695c433fc47019a757d3bf182cf1
PrinzJuliano <freack@gmx.net>
2016-08-30 16:24:43 +0200

    Changed sprite loaded

--- Java Sources/src/main/java/de/pjog/prinzJuliano/TerminalRPG/gfx/SpriteLoader.java
+++ Java Sources/src/main/java/de/pjog/prinzJuliano/TerminalRPG/gfx/SpriteLoader.java
@@ -6,8 +6,6 @@ import java.io.FileNotFoundException;
 
 import javax.imageio.ImageIO;
 
-import org.json.JSONObject;
-
 import com.googlecode.lanterna.TextCharacter;
 import com.googlecode.lanterna.TextColor;
 
